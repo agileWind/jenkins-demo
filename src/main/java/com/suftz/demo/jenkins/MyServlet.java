0a30ca8b8810352641a74050337074d755e26215
agileWind <skysgreen@qq.com>
2021-03-04 01:26:39 +0800

    第三次提交

---
+++ src/main/java/com/suftz/demo/jenkins/MyServlet.java
@@ -0,0 +1,31 @@
+package com.suftz.demo.jenkins;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Description:
+ *
+ * @author zhangchengy
+ * @version 1.0
+ * @date 2021/3/3 22:06
+ */
+@WebServlet(name = "MyServlet",urlPatterns = "/mine")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setAttribute("name","agile");
+        req.setAttribute("email","agile@suftz.com");
+        getServletContext().getRequestDispatcher("/WEB-INF/myinfo.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/myinfo.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: agile
+  Date: 2021/3/4
+  Time: 1:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>个人信息</title>
+</head>
+<body>
+${name}<br>
+${email}<br>
+</body>
+</html>
